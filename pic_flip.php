3392cd64664100d83343397745f37cb933f48071
Ian McWilliams <ian.mcwilliams@f3mmedia.co.uk>
2018-09-23 16:54:50 +0100

    refactoring and patching for directory selection

--- pic_flip.php
+++ pic_flip.php
@@ -1,5 +1,9 @@
 <?php
 
+function arrayDirectories() {
+    return removeArrayItemsByValue(scandir(path_prefix('scan')), array('.', '..', '.htaccess'));
+}
+
 function removeArrayItemsByValue($array, $values) {
     foreach ($values as $value) {
         $delete_key = array_search($value, $array);
@@ -8,62 +12,23 @@ function removeArrayItemsByValue($array, $values) {
     return $array;
 }
 
-function arrayFilenames() {
-    return removeArrayItemsByValue(scandir('images'), array('.', '..'));
+function arrayFilenames($dir) {
+    return removeArrayItemsByValue(scandir(path_prefix('scan') . '/' . $dir), array('.', '..'));
 }
 
-function currentFilename() {
+function currentFilename($dir) {
     $post_data = $_POST;
-    if (empty($post_data)) {
-        return arrayFilenames()[0];
-    } elseif (array_key_exists('nextImageButton', $post_data)) {
+    if (array_key_exists('nextImageButton', $post_data)) {
         return $post_data['nextImage'];
-    } else {
+    } elseif (array_key_exists('previousImageButton', $post_data)) {
         return $post_data['previousImage'];
-    }
-}
-
-function path_prefix($target) {
-    if ($_ENV['USER'] == 'F3M3') {
-        if ($target == 'images') {
-            return 'http://localhost:8888/projects/php/pic_flip/images';
-        } elseif ($target == 'action') {
-            return 'http://localhost:8888/projects/php/pic_flip';
-        }
     } else {
-        if ($target == 'images') {
-            return 'http://www.f3mmedia.com/imad';
-        } elseif ($target == 'action') {
-            return 'http://www.f3mmedia.com/pic_flip';
-        }
+        return arrayFilenames($dir)[0];
     }
 }
 
-function htmlOutput($currentFilename, $nextFilename, $previousFilename) {
-    $images_prefix = path_prefix('images');
-    $action_prefix = path_prefix('action');
-    $html = <<<HTML
-        <html>
-            <head>
-            </head>
-            <body>
-                <h1>$currentFilename</h1>
-                <form action="$action_prefix/pic_flip.php" method="post">
-                    <input type="hidden" name="nextImage" value="$nextFilename" />
-                    <input type="hidden" name="previousImage" value="$previousFilename" />
-
-                    <input type="submit" name="previousImageButton" value="PREVIOUS"/>
-                    <input type="submit" name="nextImageButton" value="NEXT"/>
-                </form>
-                <img src="$images_prefix/$currentFilename" />
-            </body>
-        </html>
-HTML;
-    return $html;
-}
-
-function nextFilename($currentFilename) {
-    $filenames = arrayFilenames();
+function nextFilename($currentFilename, $dir) {
+    $filenames = arrayFilenames($dir);
     $current_key = array_search($currentFilename, $filenames);
     if ($current_key == count($filenames) - 1) {
         return $filenames[0];
@@ -72,8 +37,8 @@ function nextFilename($currentFilename) {
     }
 }
 
-function previousFilename($currentFilename) {
-    $filenames = arrayFilenames();
+function previousFilename($currentFilename, $dir) {
+    $filenames = arrayFilenames($dir);
     $current_key = array_search($currentFilename, $filenames);
     if ($current_key == 0) {
         return $filenames[count($filenames) - 1];
@@ -82,8 +47,85 @@ function previousFilename($currentFilename) {
     }
 }
 
-$currentFilename = currentFilename();
-$nextFilename = nextFilename($currentFilename);
-$previousFilename = previousFilename($currentFilename);
-$out = htmlOutput($currentFilename, $nextFilename, $previousFilename);
+
+function path_prefix($target) {
+    if ($_ENV['USER'] == 'F3M3') {
+        if ($target == 'images') {
+            return 'http://localhost:8888/projects/php/pic_flip/images';
+        } elseif ($target == 'scan') {
+            return 'images';
+        } elseif ($target == 'action') {
+            return 'http://localhost:8888/projects/php/pic_flip';
+        }
+    } else {
+        if ($target == 'images') {
+            return 'http://www.f3mmedia.com/imad';
+        } elseif ($target == 'scan') {
+            return '../imad';
+        } elseif ($target == 'action') {
+            return 'http://www.f3mmedia.com/pic_flip';
+        }
+    }
+}
+
+function dirSelectHtml() {
+    $inputs_html = '';
+    foreach(arrayDirectories() as $dir_name) {
+        $inputs_html .= '<option value="' . $dir_name . '">' . $dir_name . '</option>';
+    }
+    $action_prefix = path_prefix('action');
+    return <<<HTML
+    <html>
+        <head></head>
+        <body>
+            <form action="$action_prefix/pic_flip.php" method="post">
+                <select name="dir">
+                    $inputs_html
+                </select>
+                <input type="submit" name="go_to_dir" value="Go to directory" />
+            </form>
+        </body>
+    </html>
+HTML;
+
+}
+
+function picFlipBrowserHtml() {
+    $currentDir = $_POST['dir'];
+    $currentFilename = currentFilename($currentDir);
+    $nextFilename = nextFilename($currentFilename, $currentDir);
+    $previousFilename = previousFilename($currentFilename, $currentDir);
+    $images_prefix = path_prefix('images');
+    $action_prefix = path_prefix('action');
+    $html = <<<HTML
+        <html>
+            <head>
+            </head>
+            <body>
+                <h1>$currentFilename</h1>
+                <a href="$action_prefix/pic_flip.php">return to dir list</a>
+                <form action="$action_prefix/pic_flip.php" method="post">
+                    <input type="hidden" name="nextImage" value="$nextFilename" />
+                    <input type="hidden" name="previousImage" value="$previousFilename" />
+                    <input type="hidden" name="dir" value="$currentDir" />
+
+                    <input type="submit" name="previousImageButton" value="PREVIOUS"/>
+                    <input type="submit" name="nextImageButton" value="NEXT"/>
+                </form>
+                <img src="$images_prefix/$currentDir/$currentFilename" width="200" />
+            </body>
+        </html>
+HTML;
+    return $html;
+}
+
+function htmlOutput() {
+    if (empty($_POST)) {
+        return dirSelectHtml();
+    } else {
+        return picFlipBrowserHtml();
+    }
+}
+
+$out = htmlOutput();
 echo $out;
