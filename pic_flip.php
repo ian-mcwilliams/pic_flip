3f69954526a6d48e3be258361300b6c8408cf8fc
Ian McWilliams <ian.mcwilliams@f3mmedia.co.uk>
2018-09-23 15:09:10 +0100

    make path prefix dynamic

--- pic_flip.php
+++ pic_flip.php
@@ -23,21 +23,39 @@ function currentFilename() {
     }
 }
 
+function path_prefix($target) {
+    if ($_ENV['USER'] == 'F3M3') {
+        if ($target == 'images') {
+            return 'http://localhost:8888/projects/php/pic_flip/images';
+        } elseif ($target == 'action') {
+            return 'http://localhost:8888/projects/php/pic_flip';
+        }
+    } else {
+        if ($target == 'images') {
+            return 'http://www.f3mmedia.com/imad';
+        } elseif ($target == 'action') {
+            return 'http://www.f3mmedia.com/pic_flip';
+        }
+    }
+}
+
 function htmlOutput($currentFilename, $nextFilename, $previousFilename) {
+    $images_prefix = path_prefix('images');
+    $action_prefix = path_prefix('action');
     $html = <<<HTML
         <html>
             <head>
             </head>
             <body>
                 <h1>$currentFilename</h1>
-                <img src="http://localhost:8888/projects/php/pic_flip/images/$currentFilename" />
-                <form action="http://localhost:8888/projects/php/pic_flip/sandbox.php" method="post">
+                <form action="$action_prefix/pic_flip.php" method="post">
                     <input type="hidden" name="nextImage" value="$nextFilename" />
                     <input type="hidden" name="previousImage" value="$previousFilename" />
 
                     <input type="submit" name="previousImageButton" value="PREVIOUS"/>
                     <input type="submit" name="nextImageButton" value="NEXT"/>
                 </form>
+                <img src="$images_prefix/$currentFilename" />
             </body>
         </html>
 HTML;
